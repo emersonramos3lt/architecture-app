b6505e04773d8dc24a2afdd14314ecd2931947f3
Emerson <151971499+emersonramos3lt@users.noreply.github.com>
2024-12-23 11:08:34 -0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className='mt-[8rem] bg-footerBg p-4 py-8'>
 
-        <div className='grid grid-cols-2 lg:grid-cols-4 lg:mb-[10rem] mb-[5rem]'>
+        <div className='sm:grid lg:grid-cols-4 lg:mb-[10rem] mb-[5rem] flex flex-col gap-4'>
             <div className='text-white text-[0.9rem]'>
 
                 <div className='mb-[1.2rem]'>
@@ -54,7 +54,7 @@ const Footer = () => {
                  <div className='flex flex-col lg:flex-row gap-8 lg:items-end items-center justify-center order-last lg:order-none'>
                     <span className=' lg:text-9xl text-7xl font-medium text-white'>PRISMA</span>
                     <span className='text-zinc-400 '>©2025</span>
-                    <Link href='/' className='text-white font-semibold hover:underline underline-offset-2'>
+                    <Link href='https://www.linkedin.com/in/emerson-ramos-18057333a/' className='text-white font-semibold hover:underline underline-offset-2' target='_blank'>
                         Made by Emerson
                     </Link>
                 </div>
