97286dc6243dd4461caa936cd22a247bd1e1af39
Emerson <151971499+emersonramos3lt@users.noreply.github.com>
2025-01-01 08:05:19 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,12 +4,12 @@ import Button from './Button'
 const Header = () => {
   return (
     <header className='lg:max-w-[98%] max-w-[95%] m-auto sm:mt-[5rem] mt-[3.5rem]'>
-        <h2 className='mb-[1.5rem] uppercase font-semibold text-center text-4xl sm:text-5xl lg:text-[4.7rem] lg:text-nowrap'>Timeless design, redefined spaces.</h2>
+        <h2 className='mb-[1.5rem] uppercase font-semibold text-center text-3xl sm:text-5xl lg:text-[4.7rem] lg:text-nowrap'>Timeless design, redefined spaces.</h2>
 
-        <div className='header-parallax flex justify-between items-end h-[80vh] lg:h-[90vh] w-full m-auto lg:bg-fixed bg-local'>
+        <div className='header-parallax flex justify-between items-end h-[90vh] w-full m-auto lg:bg-fixed bg-local'>
 
             <div className='flex flex-col lg:flex-row justify-between items-center p-[1.5rem] lg:gap-0 gap-4'>
-              <p className='lg:max-w-[35%] text-white font-semibold lg:text-xl text-2xl text-center lg:text-start'>Our goal is to transform ideas into impactful architectural solutions, enhancing the way people live, work, and connect with their surroundings.</p>
+              <p className='lg:max-w-[35%] text-white font-semibold lg:text-xl text-[1.1rem] text-center lg:text-start mb-5 lg:mb-auto'>Our goal is to transform ideas into impactful architectural solutions, enhancing the way people live, work, and connect with their surroundings.</p>
 
               <Button
               type='button'
